2b48e526f21700712dc3a29cd6c81f8bc932fdd9
corinthian-the <felixthuo22@gmail.com>
2026-03-12 19:28:31 +0300

    Update and rename style.js to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("form").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Your booking request has been sent! Hightours will contact you soon.");
+
+});
--- style.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(){
-
-alert("Thank you for contacting Hightours!");
-
-});
